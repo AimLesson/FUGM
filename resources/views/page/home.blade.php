1fa759f6a89f9eb03ceba613414bac447e504acd
AimLesson <113356516+AimLesson@users.noreply.github.com>
2025-03-10 14:40:02 +0700

    Reorder includes in home view to prioritize berita over essai

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -8,9 +8,9 @@
     <div class="grid grid-cols-1 md:grid-cols-3 gap-3 px-2 md:px-4 ">
         <!-- Left Side (Main Content) -->
         <div class="md:col-span-2 space-y-6 ">
-            @include('partials.berita')
             @include('partials.opini')
-            @include('partials.essai')
+            {{-- @include('partials.essai') --}}
+            @include('partials.berita')
         </div>
 
         <!-- Right Side (Sidebar/Recommendations) -->
